54483bc78651386218fdc8372124a8227f04fb22
Chia Sheng Jie <chia0360@e.ntu.edu.sg>
2017-10-15 03:24:58 +0800

    updated scene

--- scene.php
+++ scene.php
@@ -27,20 +27,19 @@
                 <a-sky src="#sky"></a-sky>
 
                 <!-- Define the Camera and Cursor -->
-                <a-entity position="0 1 4">
+                <a-entity position="0 2.2 4">
                     <a-entity camera look-controls wasd-controls>
                         <a-entity position="0 0 -3"
-                            geometry="primitive: ring; radiusInner: 0.1; radiusOuter: 0.2;"
+                            geometry="primitive: ring; radiusInner: 0.2; radiusOuter: 0.3;"
                             material="color: cyan; shader: flat"
                             cursor="maxDistance: 30; fuse: true">
-                        <a-animation begin="click" easing="ease-in" attribute="scale"
-                            fill="forwards" from="0.2 0.2 0.2" to="1 1 1" dur="150"></a-animation>
-                        <a-animation begin="fusing" easing="ease-in" attribute="scale"
-                            fill="backwards" from="1 1 1" to="0.2 0.2 0.2" dur="1500"></a-animation>
+                    <a-animation begin="click" easing="ease-in" attribute="scale"
+                        fill="forwards" from="0.2 0.2 0.2" to="1 1 1" dur="150"></a-animation>
+                    <a-animation begin="fusing" easing="ease-in" attribute="scale"
+                        fill="backwards" from="1 1 1" to="0.2 0.2 0.2" dur="1500"></a-animation>
                         </a-entity>
                     </a-entity>
-                </a-entity>
-
+              </a-entity>
                 <!-- Sample Box Assets -->
                 <a-entity id="chair" position="-1.5 0 -3">
                     <a-box width="0.05" height="0.46" depth="0.05" position="-0.21 0.23 0.23" color="#555"></a-box>
@@ -52,6 +51,8 @@
                     <a-animation attribute="rotation" begin="click" repeat="0" to="0 360 0"></a-animation>
                 </a-entity>
 
+                <a-cylinder position="4 3 0" color="crimson" height="1" radius="0.5"></a-cylinder>
+                
                 <!-- Background -->
                 <a-plane rotation="-90 0 0" width="20" height="20" color="#8E9EAB" position="0 0 0"></a-plane>
                 <!-- Walls -->
