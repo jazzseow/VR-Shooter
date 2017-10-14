85f31e405128a47d1534998205607b7d813a75eb
jazzseow <jazzseow@gmail.com>
2017-10-14 14:31:43 +0800

    Modified test.php

--- index.php
+++ index.php
@@ -3,42 +3,39 @@
         <script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script>
     </head>
     
-	<?php
-		$n = 1;
-	?>
 	<body>
-		<?php if($n==0): ?>
-			<a-scene>
-				<a-assets>
-					 <img id="texture" src="img/texture.jpg" />
-				</a-assets>
+		<a-scene>
+			<a-assets>
+				<img id="texture" src="img/texture.jpg" />
+			</a-assets>
 
-				<a-box position="-1 0.5 -3" 
-					   rotation="0 0 0" 
-					   src=#texture></a-box>
+			<a-camera position="0 0 0" rotation="45 0 0"></a-camera>
+			
+			<a-box position="-1 0.5 -3" 
+				rotation="0 0 0" 
+				src=#texture></a-box>
 							
-				<a-box position="1 0.5 -3" 
-					   rotation="0 0 0" 
-					   src=#texture></a-box>
+			<a-box position="1 0.5 -3" 
+				   rotation="0 0 0" 
+				   src=#texture></a-box>
 
-				<a-box position="-1 1 -3" 
-					   rotation="0 0 0" 
-					   src=#texture></a-box>
+			<a-box position="-1 1 -3" 
+				   rotation="0 0 0" 
+				   src=#texture></a-box>
 
-				<a-box position="-1 2 -3" 
-							rotation="0 0 0" 
-							src=#texture></a-box>
+			<a-box position="-1 2 -3" 
+						rotation="0 0 0" 
+						src=#texture></a-box>
 
-					<a-box position="-1 0.5 -1" 
-							rotation="0 0 0" 
-							src=#texture></a-box>
+			<a-box position="-1 0.5 -1" 
+					rotation="0 0 0" 
+					src=#texture></a-box>
 
-					<a-box position="-1 3 -1" 
-							rotation="0 0 0" 
-							src=#texture></a-box>
-					
-			</a-scene>
-		<?php endif;?>
+			<a-box position="-1 3 -1" 
+					rotation="0 0 0" 
+					src=#texture></a-box>
+				
+		</a-scene>
     </body>
 </html>
 
