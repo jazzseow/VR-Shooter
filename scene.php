2734611705197c40c38f393717fb3e0b5e5e1d7a
Chia Sheng Jie <chia0360@e.ntu.edu.sg>
2017-10-14 22:58:41 +0800

    updated milestone for scene.php

--- scene.php
+++ scene.php
@@ -10,24 +10,17 @@
             <a-scene>
                 <!-- Define and Import the Assets -->
                 <a-assets>
-                    <img id="texture" src="img/texture.jpg"/>
-                    <img id="sky" src="img/skybox.jpg">
-                    <a-mixin id="cube" geometry="primitive: box"></a-mixin>
-                    <a-mixin id="cube-hovered" material="color: magenta"></a-mixin>
-                    <a-mixin id="cube-selected" material="color: cyan"></a-mixin>
-                    <a-mixin id="red" material="color: red"></a-mixin>
-                    <a-mixin id="green" material="color: green"></a-mixin>
-                    <a-mixin id="blue" material="color: blue"></a-mixin>
-                    <a-mixin id="yellow" material="color: yellow"></a-mixin>
-                    <a-mixin id="sphere" geometry="primitive: sphere"></a-mixin>
+                    <img id="texture" src="assets/texture.jpg"/>
+                    <img id="sky" src="assets/skybox.jpg">
                 </a-assets>
+
                 <!-- 360-degree Image -->
                 <a-sky src="#sky"></a-sky>
 
                 <!-- Define the Camera and Cursor -->
-                <a-entity position="0 2.2 4">
+                <a-entity position="0 2 4">
                     <a-entity camera look-controls wasd-controls>
-                        <a-entity position="0 0 -3"
+                        <a-entity position="0 6 -3"
                             geometry="primitive: ring; radiusInner: 0.1; radiusOuter: 0.2;"
                             material="color: cyan; shader: flat"
                             cursor="maxDistance: 30; fuse: true">
@@ -39,11 +32,12 @@
                     </a-entity>
                 </a-entity>
 
-                <a-box position="0 2 0" 
+                <!-- Sample Box Assets -->
+                <a-box position="0 2 -3" 
                         rotation="0 0 0" 
                         src=#texture>
                     <a-animation attribute="rotation" begin="click" repeat="0" to="0 360 0"></a-animation>
-                </a-box>
+                </a-box> 
             </a-scene>
         </body>
 </html>
